9e269bb1caae6bd02b87384e438b1157dd4d4707
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-12-04 10:43:39 +0600

    navbar and footer design

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router';
 import './App.css';
+import Footer from './components/common/Footer/Footer';
+import Navbar from './components/common/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <Outlet />
+      <div className="h-screen flex flex-col">
+        <Navbar />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
