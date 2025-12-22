ae99b2f6017b482c21e08ab7c9941bf4dd9073b5
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-12-22 15:48:33 +0600

    some

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,6 +1,114 @@
+// export const getAllTours = async () => {
+//   try {
+//     const res = await fetch(`http://localhost:5000/tours`);
+
+//     if (!res.ok) {
+//       throw new Error(`Failed to fetch tours: ${res.status}`);
+//     }
+
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error('GET Error:', error.message);
+//     return { status: 'fail', message: error.message };
+//   }
+// };
+
+// export const createTour = async (formData) => {
+//   try {
+//     const res = await fetch(`http://localhost:5000/tours`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(formData),
+//     });
+
+//     if (!res.ok) {
+//       throw new Error(`POST failed: ${res.status}`);
+//     }
+
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error('POST Error:', error.message);
+//     return { status: 'fail', message: error.message };
+//   }
+// };
+
+// export const getTours = async ({ params }) => {
+//   try {
+//     const res = await fetch(`http://localhost:5000/tours/${params.id}`);
+
+//     if (!res.ok) {
+//       throw new Error(`Failed to fetch tours: ${res.status}`);
+//     }
+
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error('GET Error:', error.message);
+//     return { status: 'fail', message: error.message };
+//   }
+// };
+
+// export const updateTours = async ({ params, updatedData }) => {
+//   try {
+//     const res = await fetch(`http://localhost:5000/tours/${params}`, {
+//       method: 'PATCH',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(updatedData),
+//     });
+
+//     if (!res.ok) {
+//       throw new Error('Failed to update data');
+//     }
+
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error('GET Error:', error.message);
+//     return { status: 'fail', message: error.message };
+//   }
+// };
+
+// export const myListTours = async () => {
+//   try {
+//     const res = await fetch(`http://localhost:5000/tours`);
+
+//     if (!res.ok) {
+//       throw new Error(`Failed to fetch tours: ${res.status}`);
+//     }
+
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error('GET Error:', error.message);
+//     return { status: 'fail', message: error.message };
+//   }
+// };
+
+// export const deleteTour = async (id) => {
+//   try {
+//     const res = await fetch(`http://localhost:5000/tours/${id}`, {
+//       method: 'DELETE',
+//     });
+//     if (!res.ok) {
+//       throw new Error('Failed to delete tour');
+//     }
+
+//     return true;
+//   } catch (error) {
+//     console.error('GET Error:', error.message);
+//     return { status: 'fail', message: error.message };
+//   }
+// };
+
 export const getAllTours = async () => {
   try {
-    const res = await fetch(`http://localhost:5000/tours`);
+    const res = await fetch(`https://back-end-zeta-six.vercel.app/tours`);
 
     if (!res.ok) {
       throw new Error(`Failed to fetch tours: ${res.status}`);
@@ -16,7 +124,7 @@ export const getAllTours = async () => {
 
 export const createTour = async (formData) => {
   try {
-    const res = await fetch(`http://localhost:5000/tours`, {
+    const res = await fetch(`https://back-end-zeta-six.vercel.app/tours`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -38,7 +146,9 @@ export const createTour = async (formData) => {
 
 export const getTours = async ({ params }) => {
   try {
-    const res = await fetch(`http://localhost:5000/tours/${params.id}`);
+    const res = await fetch(
+      `https://back-end-zeta-six.vercel.app/tours/${params.id}`
+    );
 
     if (!res.ok) {
       throw new Error(`Failed to fetch tours: ${res.status}`);
@@ -54,13 +164,16 @@ export const getTours = async ({ params }) => {
 
 export const updateTours = async ({ params, updatedData }) => {
   try {
-    const res = await fetch(`http://localhost:5000/tours/${params}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedData),
-    });
+    const res = await fetch(
+      `https://back-end-zeta-six.vercel.app/tours/${params}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      }
+    );
 
     if (!res.ok) {
       throw new Error('Failed to update data');
@@ -76,7 +189,7 @@ export const updateTours = async ({ params, updatedData }) => {
 
 export const myListTours = async () => {
   try {
-    const res = await fetch(`http://localhost:5000/tours`);
+    const res = await fetch(`https://back-end-zeta-six.vercel.app/tours`);
 
     if (!res.ok) {
       throw new Error(`Failed to fetch tours: ${res.status}`);
@@ -92,9 +205,12 @@ export const myListTours = async () => {
 
 export const deleteTour = async (id) => {
   try {
-    const res = await fetch(`http://localhost:5000/tours/${id}`, {
-      method: 'DELETE',
-    });
+    const res = await fetch(
+      `https://back-end-zeta-six.vercel.app/tours/${id}`,
+      {
+        method: 'DELETE',
+      }
+    );
     if (!res.ok) {
       throw new Error('Failed to delete tour');
     }
